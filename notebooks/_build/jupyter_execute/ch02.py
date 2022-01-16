09ad550d210f01ade4d8a7fd0f87687ec1c2da72
Gyesik Lee <gslee@hknu.ac.kr>
2022-01-16 23:45:01 +0900

    everything runs fine with the directory notebooke.

--- notebooks/_build/jupyter_execute/ch02.py
+++ notebooks/_build/jupyter_execute/ch02.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Content with notebooks
+# # 프로그래밍 시작하기
 # 
 # You can also create content with Jupyter Notebooks. This means that you can include
 # code blocks and their outputs in your book.
