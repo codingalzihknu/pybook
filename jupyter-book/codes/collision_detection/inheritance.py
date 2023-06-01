efbe24d8195c80a7e9e2813aa4dc1430e3aa1907
Gyesik Lee <gslee@hknu.ac.kr>
2023-06-01 10:11:46 +0900

    update

--- jupyter-book/codes/collision_detection/inheritance.py
+++ jupyter-book/codes/collision_detection/inheritance.py
@@ -1,13 +1,3 @@
-# Basic Collision Detection
-# originally by @TokyoEdtech
-# adapted to use inheritance by @codingalzi
-
-# Topics: Collision Detection, Overlapping Coordinates,
-# Distance Checking, Axis-Aligned Bounding Box
-
-# Shoutouts:
-# 16-Bit Members Kevin, Paul, and Jan
-
 import turtle
 import math
 
@@ -25,95 +15,93 @@ shapes = ["wizard.gif", "goblin.gif", "pacman.gif", "cherry.gif", "bar.gif", "ba
 for shape in shapes:
     wn.register_shape(shape)
     
-# Sprite 클래스 
 class Sprite():
-    def __init__(self, x, y, width, height, image):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.image = image
-
-    def render(self, pen):
-        pen.goto(self.x, self.y)
-        pen.shape(self.image)
-        pen.stamp()
     
-    def is_overlapping_collision(self, other):
-        if self.x == other.x and self.y == other.y:
-            return True
-        else:
-            return False
-            
-    def is_distance_collision(self, other):
-        distance = (((self.x-other.x) ** 2) + ((self.y-other.y) ** 2)) ** 0.5
-        if distance < (self.width + other.width)/2.0:
-            return True
-        else:
-            return False
-        
-    def is_aabb_collision(self, other):
-        # Axis Aligned Bounding Box
-        x_collision = (math.fabs(self.x - other.x) * 2) < (self.width + other.width)
-        y_collision = (math.fabs(self.y - other.y) * 2) < (self.height + other.height)
-        return (x_collision and y_collision)
-        
-# Sprite 클래스 상속 
-class Character(Sprite):
-    def __init__(self, x, y, width, height, image, jump=False):
-        super().__init__(x, y, width, height, image)
-        self.jump = jump 
+  ## 생성자: 스프라이트의 위치, 가로/세로 크기, 이미지 지정
+  
+  def __init__(self, x, y, width, height, image):
+      self.x = x
+      self.y = y
+      self.width = width
+      self.height = height
+      self.image = image
 
-    def hop(self):
-        if self.jump == True:
-            self.x += 300
+  ## 스프라이트 메서드
+  
+  # 지정된 위치로 스프라이트 이동 후 도장 찍기
+  def render(self, pen):
+      pen.goto(self.x, self.y)
+      pen.shape(self.image)
+      pen.stamp()
+          
+  # 충돌 감지 방법 1: 두 스프라이트의 중심이 일치할 때 충돌 발생
+  def is_overlapping_collision(self, other):
+      if self.x == other.x and self.y == other.y:
+          return True
+      else:
+          return False
+          
+  # 충돌 감지 방법 2: 두 스프라이트 사이의 거리가 두 객체의 너비의 평균값 보다 작을 때 충돌 발생
+  def is_distance_collision(self, other):
+      distance = (((self.x-other.x) ** 2) + ((self.y-other.y) ** 2)) ** 0.5
+      if distance < (self.width + other.width)/2.0:
+          return True
+      else:
+          return False
+      
+  # 충돌 감지 방법 3: 각각의 스프라이트를 둘러썬 경계상자가 겹칠 때 충돌 발생
+  # aabb: Axis Aligned Bounding Box
+  def is_aabb_collision(self, other):
+      x_collision = (math.fabs(self.x - other.x) * 2) < (self.width + other.width)
+      y_collision = (math.fabs(self.y - other.y) * 2) < (self.height + other.height)
+      return (x_collision and y_collision)
+      
+wizard = Sprite(-128, 200, 128, 128, "wizard.gif")
+goblin = Sprite(128, 200, 108, 128, "goblin.gif")
 
+pacman = Sprite(-128, 0, 128, 128, "pacman.gif")
+cherry = Sprite(128, 0, 128, 128, "cherry.gif")
 
-wizard = Character(-128, 200, 128, 128, "wizard.gif")
-goblin = Character(128, 200, 108, 128, "goblin.gif")
+bar = Sprite(0, -400, 128, 24, "bar.gif")
+ball = Sprite(0,-200, 32, 32, "ball.gif")
 
-pacman = Character(-128, 0, 128, 128, "pacman.gif", True)
-cherry = Character(128, 0, 128, 128, "cherry.gif")
-
-bar = Character(0, -400, 128, 24, "bar.gif")
-ball = Character(0,-200, 32, 32, "ball.gif")
-
+# 스프라이트 모음 리스트
 sprites = [wizard, goblin, pacman, cherry, bar, ball]
 
+# 고블린 이동
 def move_goblin():
     goblin.x -= 64
 
+# 팩맨 이동
 def move_pacman():
     pacman.x += 30
     
-def jump_pacman():
-    pacman.hop()
-
+# 야구공 이동
 def move_ball():
     ball.y -= 24
 
+# 이벤트 처리
 wn.listen()
-wn.onkeypress(move_goblin, "Left")
-wn.onkeypress(move_pacman, "Right")
-wn.onkeypress(jump_pacman, "space")
-wn.onkeypress(move_ball, "Down")
+wn.onkeypress(move_goblin, "Left")  # 왼쪽 방향 화살표 입력
+wn.onkeypress(move_pacman, "Right") # 오른쪽 방향 화살표 입력
+wn.onkeypress(move_ball, "Down")    # 아래방향 화살표 입력
 
 while True:
     
+    # 각 스프라이트 위치 이동 및 도장 찍기
     for sprite in sprites:
         sprite.render(pen)
         
-    # Collision detection
-    if wizard.is_aabb_collision(goblin):
+    # 충돌 여부 확인
+    if wizard.is_overlapping_collision(goblin):
         wizard.image = "x.gif"
         
-    if pacman.is_aabb_collision(cherry):
+    if pacman.is_overlapping_collision(cherry):
         cherry.image = "x.gif"
         
-    if bar.is_aabb_collision(ball):
+    if bar.is_overlapping_collision(ball):
         ball.image = "x.gif"
         
-    wn.update()
-    pen.clear()
-
-# wn.mainloop()
+    wn.update() # 화면 업데이트
+    pen.clear() # 스프라이트 이동흔적 삭제
+    
