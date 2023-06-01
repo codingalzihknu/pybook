a7a37ab38d8e166042b4adfe350aa2aa7439a7ab
Gyesik Lee <gslee@hknu.ac.kr>
2023-06-01 23:30:35 +0900

    update

--- jupyter-book/codes/collision_detection/collision_detection.py
+++ jupyter-book/codes/collision_detection/collision_detection.py
@@ -17,44 +17,44 @@ for shape in shapes:
     
 class Sprite():
     
-  ## 생성자: 스프라이트의 위치, 가로/세로 크기, 이미지 지정
-  
-  def __init__(self, x, y, width, height, image):
-      self.x = x
-      self.y = y
-      self.width = width
-      self.height = height
-      self.image = image
+    ## 생성자: 스프라이트의 위치, 가로/세로 크기, 이미지 지정
 
-  ## 스프라이트 메서드
-  
-  # 지정된 위치로 스프라이트 이동 후 도장 찍기
-  def render(self, pen):
-      pen.goto(self.x, self.y)
-      pen.shape(self.image)
-      pen.stamp()
-          
-  # 충돌 감지 방법 1: 두 스프라이트의 중심이 일치할 때 충돌 발생
-  def is_overlapping_collision(self, other):
-      if self.x == other.x and self.y == other.y:
-          return True
-      else:
-          return False
-          
-  # 충돌 감지 방법 2: 두 스프라이트 사이의 거리가 두 객체의 너비의 평균값 보다 작을 때 충돌 발생
-  def is_distance_collision(self, other):
-      distance = (((self.x-other.x) ** 2) + ((self.y-other.y) ** 2)) ** 0.5
-      if distance < (self.width + other.width)/2.0:
-          return True
-      else:
-          return False
-      
-  # 충돌 감지 방법 3: 각각의 스프라이트를 둘러썬 경계상자가 겹칠 때 충돌 발생
-  # aabb: Axis Aligned Bounding Box
-  def is_aabb_collision(self, other):
-      x_collision = (math.fabs(self.x - other.x) * 2) < (self.width + other.width)
-      y_collision = (math.fabs(self.y - other.y) * 2) < (self.height + other.height)
-      return (x_collision and y_collision)
+    def __init__(self, x, y, width, height, image):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = image
+
+    ## 스프라이트 메서드
+
+    # 지정된 위치로 스프라이트 이동 후 도장 찍기
+    def render(self, pen):
+        pen.goto(self.x, self.y)
+        pen.shape(self.image)
+        pen.stamp()
+
+    # 충돌 감지 방법 1: 두 스프라이트의 중심이 일치할 때 충돌 발생
+    def is_overlapping_collision(self, other):
+        if self.x == other.x and self.y == other.y:
+            return True
+        else:
+            return False
+
+    # 충돌 감지 방법 2: 두 스프라이트 사이의 거리가 두 객체의 너비의 평균값 보다 작을 때 충돌 발생
+    def is_distance_collision(self, other):
+        distance = (((self.x-other.x) ** 2) + ((self.y-other.y) ** 2)) ** 0.5
+        if distance < (self.width + other.width)/2.0:
+            return True
+        else:
+            return False
+
+    # 충돌 감지 방법 3: 각각의 스프라이트를 둘러썬 경계상자가 겹칠 때 충돌 발생
+    # aabb: Axis Aligned Bounding Box
+    def is_aabb_collision(self, other):
+        x_collision = (math.fabs(self.x - other.x) * 2) < (self.width + other.width)
+        y_collision = (math.fabs(self.y - other.y) * 2) < (self.height + other.height)
+        return (x_collision and y_collision)
       
 wizard = Sprite(-128, 200, 128, 128, "wizard.gif")
 goblin = Sprite(128, 200, 108, 128, "goblin.gif")
@@ -96,7 +96,7 @@ while True:
     if wizard.is_overlapping_collision(goblin):
         wizard.image = "x.gif"
         
-    if pacman.is_overlapping_collision(cherry):
+    if pacman.is_distance_collision(cherry):
         cherry.image = "x.gif"
         
     if bar.is_aabb_collision(ball):
